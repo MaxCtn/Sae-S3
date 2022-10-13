df0f79c2aa8e355b577552b7123d418cde97cfc0
NeekoInAbsentia <maxime.albertelli@etu.univ-amu.fr>
2022-10-13 14:30:38 +0200

    finalisation du front de la page choix spe

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
-function setScreen(){
+function setScreen(spe){
+    if(spe == 'web'){
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que le web ?";
+    }
+    if(spe == 'reseaux'){
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que le réseau ?";
+    }
+    if(spe == 'bd'){
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que le base de donnée ?";
+    }
+    if(spe == 'algo'){
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que l'algorithmie ?";
+    }
+    if(spe == 'jsp'){
+        document.getElementById('titre_choix').innerHTML = "En quoi consiste le but Informatique ?";
+    }
+    if(spe == 'systeme'){
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que l'étude des système ?";
+    }
     document.getElementById('overlay').style.display='block'
+}
+
+function closeScreen(){
+    document.getElementById('overlay').style.display='none'
 }
