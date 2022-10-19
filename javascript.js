bac1b67944dd25252b8d77f4e48e6db7549f8f8b
NeekoInAbsentia <maxime.albertelli@etu.univ-amu.fr>
2022-10-19 17:27:31 +0200

    Ajout / début page jeux

--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
 function setScreen(spe){
     if(spe == 'web'){
         document.getElementById('titre_choix').innerHTML = "Qu'est ce que le web ?";
-        document.getElementById('p_choix').innerHTML = "Le web est ce sur quoi vous êtes actuellement (no joke). Dans le web on utilise différents language comme le HTML pour la mise en page, le CSS pour le style graphique de la page ou encore le javascript pour rendre le site plus dynamique (le javascript se rapproche plus des languages traditionnels, le HTML étant un language balisé) ";
+        document.getElementById('p_choix').innerHTML = "Le web, bah vous y êtes actuellement. Dans le web, on utilise différents langages comme le HTML pour la mise en page, le CSS pour le style graphique ou encore le javascript et le PHP pour rendre le site dynamique. Cette compétence plaira aux plus créatifs.";
     }
     if(spe == 'reseaux'){
-        document.getElementById('titre_choix').innerHTML = "Qu'est ce que le réseau ?";
-        document.getElementById('p_choix').innerHTML = "En réseaux vous étudierez la communication entre machine grâce à différents protocoles comme le TCP, l'UDP où le HTTP. Vous apprendrez aussi à lire des adresses IP ou encore comment créer un serveur et un client.";
+        document.getElementById('titre_choix').innerHTML = "Que sont les réseaux ?";
+        document.getElementById('p_choix').innerHTML = "En réseaux vous étudierez la communication entre machines grâce à différents protocoles comme le TCP, l'UDP où le HTTP. Vous apprendrez aussi à déterminer des adresses IP ou encore comment créer un serveur et un client.";
     }
     if(spe == 'bd'){
-        document.getElementById('titre_choix').innerHTML = "Qu'est ce que le base de donnée ?";
-        document.getElementById('p_choix').innerHTML = "";
+        document.getElementById('titre_choix').innerHTML = "Que sont les bases de données ?";
+        document.getElementById('p_choix').innerHTML = "Les bases de données servent à stocker des données que l'on peut ensuite récupérer et/ou manipuler. Par exemple, vous pourrez concevoir une base de données qui vous servira à établir des statistiques.";
     }
     if(spe == 'algo'){
-        document.getElementById('titre_choix').innerHTML = "Qu'est ce que l'algorithmie ?";
-        document.getElementById('p_choix').innerHTML = "";
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que la programmation ?";
+        document.getElementById('p_choix').innerHTML = "La programmation consiste à créer des algorithmes, suites d'instructions ayant un but. Vous utiliserez de nombreux langages (C++, Python, Java) qui vous permettront de raliser de nombreux projets.";
     }
     if(spe == 'jsp'){
         document.getElementById('titre_choix').innerHTML = "En quoi consiste le but Informatique ?";
-        document.getElementById('p_choix').innerHTML = "Le monde de l'informatique vous est inconnu ? Vous êtes complètement perdu et ne savez ce que vous faites là (nan sérieux ?) et bien ne vous inquietez pas, vous êtes au bon endroit ! ici les questions seront plus générales et vous permettront de vous donner une idée globale de ce qu'il se fait ici. Voici quelques compétences apprises au cours du cursus : </br> - La base de données / sql </br>- Le réseaux </br>- C++ </br>- Le développement orienté objet </br> Vous en apprendrez aussi plus sur la vie autour de BUT (l'administration, le BDE...)";
+        document.getElementById('p_choix').innerHTML = "Le monde de l'informatique vous est inconnu ? Vous êtes complètement perdu et ne savez ce que vous faites là (nan sérieux ?) et bien ne vous inquiétez pas, vous êtes au bon endroit ! Ici les questions seront plus générales et vous permettront de vous donner une idée globale de ce qu'il se fait ici. Voici quelques compétences apprises au cours du cursus : </br> - Les bases de données / sql </br>- Les réseaux </br>- La programmation fonctionnelle / C++ </br>- Le développement orienté objet / Java </br> Vous en apprendrez aussi plus sur l'organisation de votre futur diplôme.";
     }
     if(spe == 'systeme'){
-        document.getElementById('titre_choix').innerHTML = "Qu'est ce que l'étude des système ?";
-        document.getElementById('p_choix').innerHTML = "En système vous apprendrez le fonction bas des machines (le noyaux, les fonctions système, le shell) vous apprendrez comment utiliser un terminal, ce qu'est un dual boot ou encore une VM.";
+        document.getElementById('titre_choix').innerHTML = "Qu'est ce que l'étude des systèmes ?";
+        document.getElementById('p_choix').innerHTML = "En système vous apprendrez à vous sentir à l'aise dans un environnement Linux et connaitre ses avantages. Vous apprendrez notamment comment utiliser un terminal, ce qu'est un dual boot ou encore une machine virtuelle.";
     }
     document.getElementById('popup').style.display='block'
 }
