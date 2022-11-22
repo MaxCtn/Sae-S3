e8283eeb6c89bfb4d71b3893c56d5fa436e082a8
NathanVigon <nathan.vigon@etu.univ-amu.fr>
2022-11-22 16:03:02 +0100

    Responsive de la page contact, et ajout de son css

--- pages/contact.php
+++ pages/contact.php
@@ -5,7 +5,7 @@
             <!--<script src="https://kit.fontawesome.com/8e09982db4.js" crossorigin="anonymous"></script>-->
             <title>BackToBachelor - Nous Contacter</title>  
             <script src="../javascript.js"></script>
-            <link rel="stylesheet" href="../css/styleAccueil.css">
+            <link rel="stylesheet" href="../css/styleContact.css">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         </head>
@@ -13,11 +13,33 @@
             <header>
                 <div class="entete_info">
                     <div class="rubrique_menu"><a href="../index.php"><img id="return" src="../images/maison.png"></a></div>
-                    <div class="rubrique_menu"><h1 class="a_propos">Nous contacter</h1></div>
+                    <div class="rubrique_menu"><h1 class="contact_titre">Nous contacter</h1></div>
                 </div>
             </header>
             <div id="confirmeForm">Votre formulaire à bien été envoyé</div>
             <div class="menu">
+                <div class="presentation_form">
+                    <div class="titre_mail">
+                        Formulaire :
+                    </div>
+                    <div class="info_mail">
+                        <div class="nom_prenom">
+                            <input id="nom" type="text" placeholder="Nom">
+                            <input id="prenom" type="text" placeholder="Prenom">
+                        </div>
+                        <div class="mail">
+                            <input id="mail" type="mail" placeholder="Adresse E-Mail">
+                        </div>
+                    </div>
+                    <div class="p_mail">
+                        <textarea id="p_mail" placeholder="Qu'avez-vous pensés du jeu ?"></textarea>
+                    </div>
+                    <div class="valide_form">
+                        <button id="valideChoix" onclick="formConfirme()">Valider</button>
+                    </div>
+                </div>
+            </div>
+            <div class="menu2">
                 <div class="presentation_form">
                     <div class="titre_mail">
                         Formulaire :
--- pages/info.php
+++ pages/info.php
@@ -5,7 +5,7 @@
             <!--<script src="https://kit.fontawesome.com/8e09982db4.js" crossorigin="anonymous"></script>-->
             <title>BackToBachelor - A propos</title>  
             <script src="../javascript.js"></script>
-            <link rel="stylesheet" href="../css/styleAccueil.css">
+            <link rel="stylesheet" href="../css/styleInfo.css">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         </head>
@@ -54,8 +54,50 @@
                     </br></br>
                         Bonne chance et peut être à bientôt dans le BUT Informatique.
                     </p>
+                </div>
+            </div>
+
+             <div class="menu2">
+                <div class="presentation">
+                    <h2>
+                        Présentation du groupe
+                    </h2>
+                    <p>
+                        Notre groupe est constitué de 5 étudiants du BUT Informatique tous en parcours A (développement d'application) :
+                    </br>
+                        - Maxime Contino
+                    </br>
+                        - Maxime Albertelli
+                    </br>
+                        - Arthur Lecomte
+                    </br>
+                        - François Toureille
+                    </br>
+                        - Nathan Vigon
+                    </br></br>
+                        Nous avons développé ce jeu dans le cadre d'un SAE visant à créer un serious game (jeu pour apprendre) sur le thème du BUT Informatique.
+                        Nous espérons que vous vous amuserez et que cela vous aidera dans votre future orientation !
+                    </p>
+                </div>
+                <div class="presentation">
+                    <h2>
+                        Présentation du jeu
+                    </h2>
+                    <p>
+                        Bienvenue sur notre jeu "BackToBachelor" ayant pour but de vous présenter le but informatique et les débouchées que cette formation propose. 
+                        Ce jeu va vous permettre de vous faire une idée du déroulé de la formation et de ce qu'on apprends ici, en passant par le réseaux, le web, les systèmes, la base de données, l'algo ou plus général si vous n'avez aucune idée de ce qui vous intéresse.
+                        Le jeu se présente sous la forme d'une conquête de territoire dans les salles du département ou vous devrez repondre à une serie de question pour prendre possession des dites salles.
+                        Ces questions vous permettrons d'en apprendre plus sur le BUT en général et sur l'informatique.
+                    </br></br>
+                        A chaque salle vous aurez le choix entre 4 réponses possibles, un seul d'entre elles sera juste. 
+                        Vous aurez un temps imparti pour y répondre, un chat bot vous donnera des indices après un certains temps pour vous aider. 
+                        L'objectif final étant d'atteindre le BDE et prendre la place de notre cher président.
+                    </br></br>
+                        Bonne chance et peut être à bientôt dans le BUT Informatique.
+                    </p>
                 <div>
             </div>
+
         </body>
     </html>
 <?php } ?>
